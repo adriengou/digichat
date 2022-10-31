ab3f04af90ae5b3eb90848386a167674d2cedf05
Adrien Gouacide <adrien.gouacide@gmail.com>
2022-10-31 19:01:20 +0100

    added config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT = 3000;
+export const MONGO_URI = "mongodb+srv://adrieng:E2BfkWvzBVRmIraa@digichat.otbhvo9.mongodb.net/chat";
+export const SECRET = "ce3d96aeb39f40b999abd4f38b3bdcafa16e140a"
+export const TEST = false
+export const ADMIN_CODE = "b7809f186532638764324a6893284734"
